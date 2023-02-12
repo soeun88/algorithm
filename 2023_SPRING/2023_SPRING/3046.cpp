f22509173c8562e9ce4a6fc01b132caaaf6b8758
soeun88 <soeun0828@icloud.com>
2023-02-12 21:26:24 +0900

    2023-02-12

---
+++ 2023_SPRING/2023_SPRING/3046.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	int R1, R2, S;
+
+	cin >> R1 >> S;
+
+	R2 = 2 * S - R1;
+
+	cout << R2;
+
+
+}
---
+++ 2023_SPRING/2023_SPRING/6986.cpp
@@ -0,0 +1,43 @@
+//미완
+#include<iostream>
+using namespace std;
+
+int main() {
+	int N, K;
+	double score[100000] = {};
+	double trimmed[100000] = {};
+	double adjusted[100000] = {};
+	double trim = 0, adjust = 0;
+
+	cin >> N >> K;
+
+	for (int i = 0; i < N; i++) {
+		cin >> score[i];
+	}
+
+	//절사평균
+	for (int i = 0; i < K; i++) {
+		score[i] = score[i + 1];
+	}
+
+	for (int i = 0; i < N - K; i++) {
+		trim += score[i];
+	}
+	trim /= (N - 2 * K);
+
+
+	//보정평균
+	for (int i = 0; i < K; i++) {
+		score[i] = score[K];
+		score[N - i] = score[N - K];
+	}
+
+	for (int i = 0; i < N; i++) {
+		adjust += score[i];
+	}
+	adjust /= N;
+
+	cout << trim << endl;
+	cout << adjust << endl;
+}
+
