b01fcb41ef29fbcdcd87f85737b260a7f4a13822
soeun88 <soeun0828@icloud.com>
2023-02-15 22:43:48 +0900

    2023-02-15

---
+++ 2023_SPRING/2023_SPRING/15894.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	long long n, result;
+
+	cin >> n;
+
+	result = 4 * n;
+
+	cout << result;
+
+
+}
---
+++ 2023_SPRING/2023_SPRING/2839.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	int N, result;
+
+	cin >> N;
+
+	if (N % 5) result = N / 5;
+	else {
+		if ((N % 5) % 3) result = N / 5 + (N % 5) / 3;
+		else if (N % 3) result = N / 3;
+		else result = -1;
+	}
+
+	cout << result << endl;
+
+}
