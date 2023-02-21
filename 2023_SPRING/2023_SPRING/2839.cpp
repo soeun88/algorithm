d8fd18c233c2e8ed852f24e9b814f86cb0f7a1b9
soeun88 <soeun0828@icloud.com>
2023-02-21 21:35:26 +0900

    2023-02-21

--- 2023_SPRING/2023_SPRING/2839.cpp
+++ 2023_SPRING/2023_SPRING/2839.cpp
@@ -6,14 +6,15 @@ int main() {
 
 	cin >> N;
 
-	if (N % 5) result = N / 5;
+	if (N % 5==0) result = N / 5;
 	else {
-		if ((N % 5) % 3) result = N / 5 + (N % 5) / 3;
-		else if (N % 3) result = N / 3;
+		if ((N % 5) % 3==0) result = N / 5 + (N % 5) / 3;
+		else if (N % 3==0) result = N / 3;
 		else result = -1;
 	}
 
 	cout << result << endl;
 
 }
+
 
