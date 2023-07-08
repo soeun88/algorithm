f209a72a2862dba5be0c4a8a325052a557ddb226
soeun88 <soeun0828@icloud.com>
2023-07-09 00:34:22 +0900

    2023-07-09

--- 2023_SUMMER/2023_SUMMER/5430.cpp
+++ 2023_SUMMER/2023_SUMMER/5430.cpp
@@ -1,47 +1,81 @@
 #include<iostream>
+#include<algorithm>
+#include<deque>
+#include<string>
 using namespace std;
 
-#include<queue>
-queue<int>Xq;
+deque<int>Xq;
 
 int main() {
 	int T, n;
 	string p;
-	int X[100] = { 0, };
-	int pLen = 0;
-	int XLen = 0;
+	int pLen = 0, XLen = 0;
+	string X, num;
+	string answer;
+	int cnt, size = 0;
+	int R, D;
 
 	cin >> T;
-	
+	cin.ignore();
+
 	for (int i = 0; i < T; i++) {
 		cin >> p;
 		cin >> n;
+		cin >> X;
 
-		for (int j = 0; j < n; j++) {
-			cin >> X[j];
-			Xq.push(X[i]);
+		num = "";
+		XLen = X.length();
+		for (int j = 1; j < XLen; j++) {
+			if (X[j] == ']' || X[j] == ',') {
+				if (!num.empty()) {
+					Xq.push_back(stoi(num));
+					num = "";
+				}
+			}
+			else num += X[j];
 		}
 
 		pLen = p.length();
-		XLen = Xq.size();
+		R = 0;
+		D = 0;
 		for (int k = 0; k < pLen; k++) {
 			if (p[k] == 'R') {
-				reverse(begin(X),end(X));
+				if (R == 0) R = 1;
+				else R = 0;
 			}
 			else {
-				if (XLen == 0) {
+				if (Xq.empty()) {
 					cout << "error" << endl;
+					D = 1;
 					break;
 				}
 				else {
-					Xq.pop();
+					if (R == 0) Xq.pop_front();
+					else Xq.pop_back();
 				}
 			}
 		}
 
-		cout << X << endl;
+		if (D != 1) {
+			cnt = 0;
+			cout << '[';
+			while (!Xq.empty()) {
+				if (cnt != 0) cout << ',';
+				if (R == 0) {
+					cout << Xq.front();
+					Xq.pop_front();
+				}
+				else {
+					cout << Xq.back();
+					Xq.pop_back();
+				}
+				cnt++;
+			}
+			cout << ']' << endl;
+		}
 	}
-
-
 }
+
+
+
 
