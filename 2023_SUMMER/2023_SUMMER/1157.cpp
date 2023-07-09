dc1600f0c1daea48a12c9be0bf3ceb9eb91ac679
soeun88 <soeun0828@icloud.com>
2023-07-10 00:08:55 +0900

    2023-07-10

--- 2023_SUMMER/2023_SUMMER/1157.cpp
+++ 2023_SUMMER/2023_SUMMER/1157.cpp
@@ -1,27 +1,43 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
 int main() {
 	string word;
-	int len = 0;
-	string lower = "abcdefghijklmnopqrstuvwxyz";
-	string upper = "ABCDEFGHIJKLMNOPQRXTUVWXYZ";
-	int count[26] = { 0, };
-	char max = 'a';
+	int len, n;
+	int count[26] = { 0 };
+	int index = 0, max = 0;
+	int flag = 0;
 
 	cin >> word;
 
 	len = word.length();
 
-	for (int j = 0; j < len; j++) {
-		for (int i = 0; i < 26; i++) {
-			if (word[j] == lower[i] || word[j] == upper[i])count[i]++;
+	for (int i = 0; i < len; i++) {
+		n = word.at(i);
+		if (n < 97) count[n - 65]++;
+		else count[n - 97]++;
+	}
+
+	for (int i = 0; i < 26; i++) {
+		if (count[i] > max) {
+			max = count[i];
+			index = i;
 		}
 	}
 
 	for (int i = 0; i < 26; i++) {
-
+		if (count[i] == max) {
+			flag++;
+			if (flag >= 2) {
+				cout << '?' << endl;
+				return 0;
+			}
+		}
 	}
 
 
+	cout << (char)(index + 65) << endl;
+
+	return 0;
 }
