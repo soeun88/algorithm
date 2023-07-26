6d72f056d320d143e94cad76376201bc2197a0c2
soeun88 <soeun0828@icloud.com>
2023-07-26 22:42:19 +0900

    2023-07-26

--- 2023_SUMMER/2023_SUMMER/2852.cpp
+++ 2023_SUMMER/2023_SUMMER/2852.cpp
@@ -5,88 +5,95 @@ using namespace std;
 
 int main() {
 	int N;
-	int team[100] = { 0, };
-	string time[100] = {};
-	string MM[100] = {}, SS[100] = {};
-	int MM1 = 0, SS1 = 0, MM2 = 0, SS2 = 0;
-	int flag1 = 0, flag2 = 0;
-
+	string time;
+	int team;
+	int MM, SS;
+	int Ascore = 0, Bscore = 0;
+	int preMM = 0, preSS = 0;
+	int MM1 = 0, SS1 = 0;
+	int MM2 = 0, SS2 = 0;
 
 	cin >> N;
 
 	for (int i = 0; i < N; i++) {
-		cin >> team[i] >> time[i];
-	}
+		cin >> team >> time;
 
-	for (int i = 0; i < N; i++) {
-		MM[i] = time[i].substr(0, 2);
-		SS[i] = time[i].substr(3, 2);
-	}
+		MM = stoi(time.substr(0, 2));
+		SS = stoi(time.substr(3, 2));
 
-	for (int i = 0; i < N; i++) {
-		if (N == 1) {
-			if (team[i] == 1) {
-				if (SS[i] == "00") {
-					MM1 = 48 - stoi(MM[i]);
+		
+
+		if (i >= 1) {
+			if (Ascore > Bscore) {
+				MM1 += (MM - preMM);
+				SS1 += (SS - preSS);
+				if (SS1 < 0) {
+					MM1--;
+					SS1 += 60;
 				}
-				else {
-					MM1 = 48 - stoi(MM[i]) - 1;
-					SS1 = 60 - stoi(SS[i]);
-				}
-			}
-			else {
-				if (SS[i] == "00") {
-					MM2 = 48 - stoi(MM[i]);
-				}
-				else {
-					MM2 = 48 - stoi(MM[i]) - 1;
-					SS2 = 60 - stoi(SS[i]);
+				if (SS1 >= 60) {
+					MM1++;
+					SS1 -= 60;
 				}
 			}
-		}
-		else {
-			if (team[i] == 1) {
-				flag1++;
-				if (SS[i] == "00") {
-					MM1 += (stoi(MM[i + 1]) - stoi(MM[i]));
+			else if (Bscore > Ascore) {
+				MM2 += (MM - preMM);
+				SS2 += (SS - preSS);
+				if (SS2 < 0) {
+					MM2--;
+					SS2 += 60;
 				}
-				else {
-					MM1 += (stoi(MM[i + 1]) - stoi(MM[i]) - 1);
-					SS1 += (stoi(SS[i + 1]) - stoi(SS[i]));
-					if (SS1 < 0) SS1 += 60;
-				}
-			}
-			else {
-				flag2++;
-				if (SS[i] == "00") {
-					MM2 += (stoi(MM[i + 1]) - stoi(MM[i]));
-				}
-				else {
-					MM2 += (stoi(MM[i + 1]) - stoi(MM[i]) - 1);
-					SS2 += (stoi(SS[i + 1]) - stoi(SS[i]));
-					if (SS2 < 0)SS2 += 60;
+				if (SS2 >= 60) {
+					MM2++;
+					SS2 -= 60;
 				}
 			}
 		}
+
+		preMM = MM;
+		preSS = SS;
+
+		if (team == 1)Ascore++;
+		else Bscore++;
 	}
 
+	if (Ascore > Bscore) {
+		MM1 += (48 - preMM);
+		SS1 += (0 - preSS);
+		if (SS1 < 0) {
+			MM1--;
+			SS1 += 60;
+		}
+		if (SS1 >= 60) {
+			MM1++;
+			SS1 -= 60;
+		}
+	}
+	else if (Bscore > Ascore) {
+		MM2 += (48 - preMM);
+		SS2 += (0 - preSS);
+		if (SS2 < 0) {
+			MM2--;
+			SS2 += 60;
+		}
+		if (SS2 >= 60) {
+			MM2++;
+			SS2 -= 60;
+		}
+	}
 
 	cout.width(2);
 	cout.fill('0');
-	cout << MM1;
-	cout.width(1);
-	cout << ':';
+	cout << MM1 << ":";
+	cout.width(2);
+	cout.fill('0'); 
+	cout << SS1 << '\n';
 	cout.width(2);
 	cout.fill('0');
-	cout << SS1;
-	cout << '\n';
+	cout << MM2 << ":";
 	cout.width(2);
-	cout.fill('0');
-	cout << MM2;
-	cout.width(1);
-	cout << ':';
-	cout.width(2);
-	cout.fill('0');
-	cout << SS2;
-	cout << '\n';
+	cout.fill('0'); 
+	cout << SS2 << '\n';
+
+
 }
